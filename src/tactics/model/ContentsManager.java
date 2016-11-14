f539912d00a2872a3684670f8bb9de9b469515e1
admin <admin@192.168.10.20>
2016-11-14 18:28:10 +0900

    tactics(전략게시판) 추가

---
+++ src/tactics/model/ContentsManager.java
@@ -0,0 +1,37 @@
+package tactics.model;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import tactics.model.pojo.Tactics;
+
+@Component
+public class ContentsManager {
+	@Autowired
+	SqlSessionFactory fac;
+	
+	public boolean write(Tactics tac) {
+		SqlSession sql = fac.openSession();
+		boolean r = sql.insert("tactics.write", tac)==1?true:false;
+		sql.close();
+		return r;
+	}
+
+	public List listPage(int page, int length) {
+		Integer[] i = {page, length};
+		SqlSession sql = fac.openSession();
+		List list = sql.selectList("tactics.page");
+		return list;
+	}
+
+	public Tactics read(int num) {
+		SqlSession sql = fac.openSession();
+		Tactics tac = sql.selectOne("tactics.read", num);
+		return tac;
+	}
+}
