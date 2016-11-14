20efdbdbe6dd60efe068863224d84b3a55b71587
admin <admin@192.168.10.15>
2016-11-14 17:04:20 +0900

    에러 수정

--- src/staticData/controller/ChampDbController.java
+++ src/staticData/controller/ChampDbController.java
@@ -1,5 +1,14 @@
 package staticData.controller;
 
-public class ChampDbController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/db")
+public class ChampDbController {
+	
+	@RequestMapping("/showChampDB")
+	public String showChampDB() {
+		return null;
+	}
 }
