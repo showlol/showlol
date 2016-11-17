4dc96aed745318198734195c3708f27afa918a56
Sungwook <Sungwook@112.171.57.11>
2016-11-18 06:34:13 +0900

    성욱: 일단 룬 이미지만 불러오기

---
+++ src/tactics/controller/RunImageController.java
@@ -0,0 +1,31 @@
+package tactics.controller;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RunImageController {
+	@Autowired
+	SqlSessionFactory fac;
+	
+	@RequestMapping("/rune")
+	public String rune(){
+		SqlSession sql = fac.openSession();
+		List<Integer> list = new ArrayList<>();
+		list.add(5003);
+		list.add(5007);
+		list.add(5009);
+		list.add(5011);
+		list.add(5013);
+		List<String> img = sql.selectList("rune.image", list);
+		System.out.println(img.toString());
+		return "tactics/rune";
+	}
+}
