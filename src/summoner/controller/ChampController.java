d825a600893ee19c5160cc826eb62806811e499d
ganzi <ganzi@ganzi-PC>
2016-11-23 22:45:32 +0900

    챔피언 탭 추가

---
+++ src/summoner/controller/ChampController.java
@@ -0,0 +1,26 @@
+package summoner.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import summoner.model.ChampService;
+
+@Controller
+public class ChampController {
+	
+	@Autowired
+	ChampService csvc;
+	
+	@RequestMapping("/summoner/champion")
+	public ModelAndView champion(String userName) {
+		ModelAndView mav = new ModelAndView("/summoner/champion");
+		ArrayList list = csvc.getChampData(0);
+		mav.addObject("list", list);
+		
+		return mav;
+	}
+}
---
+++ src/summoner/model/ChampService.java
@@ -0,0 +1,38 @@
+package summoner.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class ChampService {
+	
+	@Autowired
+	BasicService bsvc;
+	
+	public ArrayList getChampData(long id) {
+		RestTemplate rt = new RestTemplate();
+		//String url = "https://kr.api.pvp.net/api/lol/kr/v1.3/stats/by-summoner/"+id+"/ranked?season=SEASON2016&api_key=RGAPI-23040d79-d49d-4850-a32e-a238bbe04e09";
+		String url = "https://kr.api.pvp.net/api/lol/kr/v1.3/stats/by-summoner/"+bsvc.sinfo.get("id")+"/ranked?season=SEASON2016&api_key=RGAPI-23040d79-d49d-4850-a32e-a238bbe04e09";
+		LinkedHashMap map = rt.getForObject(url, LinkedHashMap.class);
+		ArrayList champs = (ArrayList)map.get("champions");
+		ArrayList res = new ArrayList<>();
+		for(int i=0; i<champs.size(); i++) {
+			LinkedHashMap data = (LinkedHashMap)champs.get(i);
+			int cid = (int)data.get("id");
+			String cname = bsvc.getChampNameKR(cid);
+			String iname = bsvc.getChampName(cid);
+			if(cname != null && iname != null) {
+				data.put("cname", cname);
+				data.put("iname", iname);
+				res.add(data);
+			}
+		}
+		
+		return res;
+	}
+}
